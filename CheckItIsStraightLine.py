d1c68ea6043131a6208b43190f5e323214bebb7b
Invictus <max.25jr@gmail.com>
2023-06-25 17:39:19 -0400

    Updates

--- CheckItIsStraightLine.py
+++ CheckItIsStraightLine.py
@@ -1,3 +1,5 @@
+# You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents
+# the coordinate of a point. Check if these points make a straight line in the XY plane.
 class Solution:
     def checkStraightLine(self, coordinates: list[list[int]]) -> bool:
         xDelta = coordinates[1][0] - coordinates[0][0] # Difference between x2 and x1
