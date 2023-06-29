f82f09417a3668292988a64979bf6da441b8e8c2
Invictus <max.25jr@gmail.com>
2023-06-29 14:47:37 -0400

    Updates

---
+++ RemoveDuplicatesfromSortedArray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+
+
+        writingIndex = 1  # The first presence of an integer
+        readingIndex = len(nums)
+
+        for i in range(1, readingIndex):  # To start with 0 element during founding the unique element.
+            if nums[i - 1] != nums[i]:
+                nums[writingIndex] = nums[i]
+                writingIndex += 1
+        return writingIndex  # the number of unique elements in nums
+
+example = Solution()
+print(example.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+
