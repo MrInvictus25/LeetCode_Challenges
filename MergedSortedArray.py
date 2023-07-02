c676e6113b82460e8d8f8fb01d12a3899a96d895
Invictus <max.25jr@gmail.com>
2023-07-02 15:25:58 -0400

    Updates

--- MergedSortedArray.py
+++ MergedSortedArray.py
@@ -10,22 +10,24 @@ class Solution:
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         #([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
         nums1Copy = nums1[:m]  # Coping the first m values of nums1
-
+        # [1, 2, 3]
         ReadPointer1 = 0  # Initialization read pointer to the beginning of nums1Copy
         ReadPointer2 = 0  # Initialization read pointer to the beginning of nums2
         WritePointer = 0  # Initialization read pointer to the beginning of nums1
 
         for WritePointer in range(n + m):  # The length of nums1 list
+            print(WritePointer)
+            print('=' * 50)
             if ReadPointer2 >= n or (ReadPointer1 < m and nums1Copy[ReadPointer1] <= nums2[ReadPointer2]):  # Reading values from nums1Copy
-                nums1[WritePointer] = nums1Copy[ReadPointer1]
-                print(nums1[WritePointer])
+                nums1[WritePointer] = nums1Copy[ReadPointer1]  # The case when we keep element from the nums1 list
+                print('This is WritePointer: ', nums1[WritePointer])
                 print('=' * 50)
-                print(nums1Copy[ReadPointer1])
+                print('This is ReadPointer: ', nums1Copy[ReadPointer1])
                 print('=' * 50)
 
                 ReadPointer1 += 1
             else:
-                nums1[WritePointer] = nums2[ReadPointer2]
+                nums1[WritePointer] = nums2[ReadPointer2]  # The case when we add element from the nums2 list
                 ReadPointer2 += 1
 
         WritePointer += 1
