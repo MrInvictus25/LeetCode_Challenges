44eca574e8a4571e6a31232b5364f8a8668eaac1
Invictus <max.25jr@gmail.com>
2023-10-24 16:56:41 -0400

    Updates

--- AddBinary.py
+++ AddBinary.py
@@ -29,6 +29,8 @@ class Solution:
             # During the loop, bits from the same position in a and b are added to carriage.
             # If carriage becomes 2 or greater, it indicates that a carry is needed to the next bit.
             # If the condition is not met, it means no carry is generated for the current bit addition, so a '0' is appended to the result list.
+            # This is a typical approach to adding binary numbers, where the carry is propagated to the next
+            # bit when the sum of two bits at the same position exceeds 1
 
         if carriage == 1:
             result.append('1')
